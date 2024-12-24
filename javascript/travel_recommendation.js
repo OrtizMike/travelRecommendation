cb7099cf28b92ce9aaae470c41d174aea81d901f
Miguel Ortiz <miguel_ortiz22@apple.com>
2024-12-23 22:52:53 -0600

    Show random countries

--- javascript/travel_recommendation.js
+++ javascript/travel_recommendation.js
@@ -8,15 +8,69 @@ btnReset.addEventListener("click", resetRecomendations);
 
 const apiUrl = "/travel_recommendation_api.json"
 
-function searchRecommendation() {
+function searchRecommendation(event) {
+  const searchInput = document.getElementById("searchInput")
+  const searchKeyword = searchInput.value.toLowerCase()
+  if (!searchKeyword || searchKeyword.length === 0) {
+    resetRecomendations()
+    return
+  }
+
   fetch(apiUrl)
     .then(response => response.json())
-    .then(data => {
-      
+    .then(result => {
+      recommendationContainer.innerHTML = ""
+      let data
+
+
+      if( searchKeyword.includes("beach")) data = result.beaches
+      if( searchKeyword.includes("temple")) data = result.temples
+      if( searchKeyword.includes("countries") || searchKeyword.includes("country")) data = result.countries[Math.floor(Math.random() * 3)].cities
+
+      data.forEach(destination => {
+        const destinationContainer = document.createElement('div')
+        destinationContainer.classList.add('destination-card');
+        const destinationPhotoContainer = document.createElement('div')
+        destinationPhotoContainer.classList.add('destination-photo');
+        const destinationPhoto = document.createElement('img')
+        destinationPhoto.src = `/img/${destination.imageUrl}`
+        const destinationInfo = document.createElement('div')
+        destinationInfo.classList.add('destination-info');
+        const destinationName = document.createElement('span')
+        destinationName.classList.add('destination-name');
+        destinationName.textContent = destination.name
+        const destinationDescription = document.createElement('span')
+        destinationDescription.classList.add('destination-description');
+        destinationDescription.textContent = destination.description
+        const destinationVisit = document.createElement('input')
+        destinationVisit.type = "button"
+        destinationVisit.value = "Visit"
+        destinationVisit.classList.add('btn')
+        destinationVisit.classList.add('destination-visit')
+
+
+        destinationPhotoContainer.appendChild(destinationPhoto)
+        destinationInfo.appendChild(destinationName)
+        destinationInfo.appendChild(destinationDescription)
+        destinationInfo.appendChild(destinationVisit)
+        destinationContainer.appendChild(destinationPhotoContainer)
+        destinationContainer.appendChild(destinationInfo)
+        recommendationContainer.appendChild(destinationContainer)
+      })
+
+      // Countries
+
+
     })
+    .catch(error => {
+      console.log('Error:', error);
+      resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
 }
 
+
+
 function resetRecomendations() {
-  document.getElementById("destinationSearch").value = ""
+  document.getElementById("searchInput").value = ""
   recommendationContainer.innerHTML = ""
 }
